60d3fb9320ba4d72516ad66490d13ac3f3c23ae1
TheOverridden <lincmoore404@proton.me>
2026-09-18 07:06:36 -0500

    Add polished startup transition

--- src/core/boot.js
+++ src/core/boot.js
@@ -1,5 +1,18 @@
-installGuardianModes();
+function finishBoot(){
+ const root=document.documentElement,screen=T('bootScreen');
+ requestAnimationFrame(()=>requestAnimationFrame(()=>{
+  root.classList.remove('booting');root.classList.add('boot-ready');
+  if(!screen)return;
+  const remove=()=>screen.remove();
+  screen.addEventListener('transitionend',remove,{once:true});
+  setTimeout(remove,1200);
+ }));
+}
 
-init();
-
-showTheSeventhEmberUpdates();
+try{
+ installGuardianModes();
+ init();
+ showTheSeventhEmberUpdates();
+}finally{
+ finishBoot();
+}
